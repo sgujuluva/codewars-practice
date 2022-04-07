d7b89ab19e40f189f2c335abcdb4747080c2c60b
sgujuluva <sanghee04@gmail.com>
2022-04-07 15:54:02 +0200

    array concatination

---
+++ level8/sheep-exercise.js
@@ -0,0 +1,15 @@
+/* If you can't sleep, just count sheep!!
+
+Task:
+Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers. */
+
+
+var countSheep = function (num){
+    //your code here
+    let result = "";
+    for (i = 1; i <= num ;i++){
+   result += `${i} sheep....`;
+    }
+    return result;
+  }
+console.log(countSheep(5));
